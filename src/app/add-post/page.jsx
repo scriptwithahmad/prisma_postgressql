0b533d10cd7a4c1bc627d9eee163bb20063ed27d
Ahmad <scriptwithahmad@gmail.com>
2024-08-30 14:35:04 +0500

    add new post with frontend form

--- src/app/add-post/page.jsx
+++ src/app/add-post/page.jsx
@@ -2,42 +2,68 @@
 import React, { useState } from 'react'
 
 const Page = () => {
-     // State hooks for title and content
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  // State hooks for title and content
+  const [formData, setFormData] = useState({
+    title: '',
+    content: '',
+  })
+  console.log(formData)
+
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   // Handler for form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    
 
-    setTitle('');
-    setContent('');
+    try {
+
+      fetch('/api/add-post', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+        , body: JSON.stringify({ formData })
+      })
+
+      setFormData({
+        title: '',
+        content: '',
+      })
+    } catch (error) {
+      console.log(error)
+    }
+
   };
   return (
-    <form onSubmit={handleSubmit}>
-    <div>
-      <label htmlFor="title">Title:</label>
-      <input
-        type="text"
-        id="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
-    </div>
-    <div>
-      <label htmlFor="content">Content:</label>
-      <textarea
-        id="content"
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-        required
-      />
-    </div>
-    <button type="submit">Submit</button>
-  </form>
+    <form onSubmit={handleSubmit} className='grid place-items-center gap-4 pt-8'>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          name='title'
+          value={formData.title}
+          onChange={handleChange}
+          required
+          className='border border-gray-300 rounded px-2 py-1 bg-gray-700'
+        />
+      </div>
+      <div>
+        <label htmlFor="content">Content:</label>
+        <textarea
+          id="content"
+          value={formData.content}
+          onChange={handleChange}
+          name='content'
+          required
+          className='border border-gray-300 rounded px-2 py-1 bg-gray-700'
+        />
+      </div>
+      <button type="submit" className='border border-gray-300 rounded px-2 py-1'>Submit</button>
+    </form>
   )
 }
 
--- src/app/api/add-post/route.js
+++ src/app/api/add-post/route.js
@@ -1,7 +1,23 @@
 import { NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
 
 export async function POST(request) {
-    const res = await request.json(); 
-    console.log(res)
-    return NextResponse.json(res);
+  const res = await request.json();
+
+  const { title, content } = res.formData;
+
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: true,
+      author: {
+        create: {
+          name: "Usman Ashfaq",
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(res);
 }
